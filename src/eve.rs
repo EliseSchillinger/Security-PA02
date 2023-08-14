ef303dc10b4446762283d08fefaac0d626a068f2
Elise Schillinger <37603918+EliseSchillinger@users.noreply.github.com>
2023-08-13 21:24:36 -0500

    Add files via upload

---
+++ src/eve.rs
@@ -0,0 +1,37 @@
+pub fn baby_eve(alice_broadcasts: u64, bob_broadcasts: u64, public_base: u64) -> [u64; 3]
+{
+    // Purpose:     Crack baby DH
+    // Parameters:  alice's broadcast, bob's broadcast, and the public base
+    // User-input:  None
+    // Prints:      Nothing
+    // Returns:     Should return an array of 3 ints:
+    //              Alice's secret, Bob's secret, shared secret
+    // Modifies:    Nothing
+    // Calls:       ?
+    // Tests:       unit_test_babydh.rs
+    // Status:      Done, correct, but is it ideal?
+    [0, 0, 0]
+}
+
+// This function is bonus (see the README.md for details)
+// pub fn big_eve(
+//     alice_broadcasts: i64,
+//     bob_broadcasts: i64,
+//     public_base: i64,
+//     public_modulus: i64,
+// ) -> [u64; 3] {
+//     // Purpose:      Crack real DH (albeit not with huge numbers)
+//     // Parameters:   Alice's broadcast, Bob's broadcast, the public base and modulus of DH.
+//     // User-input:   None
+//     // Prints:       Nothing
+//     // Returns:      Should return an array of 3 ints:
+//     //               Alice's secret, Bob's secret, shared secret
+//     // Modifies:     Nothing
+//     // Calls:        ?
+//     // Test:         ./unit_tests/unit_test_babydh.rs
+//     // Status:       TODO delete the 0 placeholders, and replace with real computations
+//     let alice_secret = 0;
+//     let bob_secret = 0;
+//     let mutual_secret = 0;
+//     [alice_secret as i64, bob_secret as i64, mutual_secret as i64]
+// }
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod eve;
+
+fn main() {
+    // Purpose:    Driver for DH problems
+    // Parameters: None
+    // User Input: If no args, input dec numbers
+    // Prints:     If no args, then print result
+    // Returns:    Nothing
+    // Modifies:   Nothing
+    // Calls:      ?
+    // Tests:      arg_tests/ and stdio_tests/
+    // Status:     Student does this
+}
